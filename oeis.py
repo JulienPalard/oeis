7233d5f4d298c3921d536035352fd39b49a8e954
Grégoire Masse <44845228+valugit@users.noreply.github.com>
2019-11-18 16:23:48 +0100

    Added requirement Black (#17)

--- oeis.py
+++ oeis.py
@@ -12,7 +12,7 @@ __version__ = "0.0.1"
 def parse_args():
     parser = argparse.ArgumentParser(description="Print a sweet sequence")
     parser.add_argument(
-        "sequence", type=str, help="Define the sequence to run (e.g.: A181391)",
+        "sequence", type=str, help="Define the sequence to run (e.g.: A181391)"
     )
     parser.add_argument(
         "--limit",
@@ -60,7 +60,6 @@ def A181391(start=0, limit=20, plot=False):
 
 
 def A006577(n):
-
     if n == 1:
         return 0
 
@@ -215,7 +214,7 @@ def A008592(start, limit):
     i = 0
     print("A008592 sequence:")
     while i < end:
-        my_list.append(i*10)
+        my_list.append(i * 10)
         i += 1
     return my_list[start:end]
 
@@ -241,8 +240,8 @@ def A000041(n):
 
 
 def main():
-
     args = parse_args()
+
     if args.sequence == "A008592":
         return A008592(args.start, args.limit)
     elif args.sequence == "A181391":
--- tests/test_A181391.py
+++ tests/test_A181391.py
@@ -3,6 +3,10 @@ from hypothesis import given
 from hypothesis.strategies import integers
 
 
-@given(integers(min_value=1, max_value=20000))
-def test_sequence_length(x):
-    assert len(A181391(limit=x)) == x
+def test_sequence():
+    assert A181391() == [0, 0, 1, 0, 2, 0, 2, 2, 1, 6, 0, 5, 0, 2, 6, 5, 4, 0, 5, 3]
+
+
+@given(integers(min_value=0, max_value=20000), integers(min_value=1, max_value=20000))
+def test_sequence_length(x, y):
+    assert len(A181391(x, y)) == y
