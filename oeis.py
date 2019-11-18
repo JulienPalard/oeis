106158253afd5d46a673f06f047c08be20d0e2c8
Anthony <anthonyd-m@live.com>
2019-11-18 14:22:05 +0100

    added euler totient function

--- oeis.py
+++ oeis.py
@@ -1,7 +1,9 @@
 import argparse
+import math
 
 parser = argparse.ArgumentParser(description='')
 
+parser.add_argument('sequence')
 parser.add_argument('--start', type= int, default=0)
 parser.add_argument('--limit', type= int, default=17)
 
@@ -14,5 +16,17 @@ def countdown():
             result.append(n)
     
     return result
+
+def phi(n):
+    numbers = []
+    i = 0
+    for i in range(n):
+        if math.gcd(i, n) == 1:
+            numbers.append(i)
+    return len(numbers)
+
+def main():
+    if args.sequence == "A000010":
+        return [phi(x) for x in range (1, args.limit)]
             
-print(countdown()[args.start:args.start + args.limit])
+print(main())
