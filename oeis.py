538c286ad117b2decd4a44d90e527e450aea6642
TyomaMtl <scollioudfree.fr@gmail.com>
2019-11-19 14:22:29 +0100

    fix indentation

--- oeis.py
+++ oeis.py
@@ -106,16 +106,16 @@ def A000290(start=0, limit=20):
 
 
 @oeis
-def A001462(n=6): 
-  
-    dp = [0] * (n + 1) 
-   
+def A001462(n=6):
+
+    dp = [0] * (n + 1)
+
     dp[1] = 1
-  
-    for i in range(2, n + 1):  
-      
-        dp[i] = 1 + dp[i - dp[dp[i - 1]]]  
-    
+
+    for i in range(2, n + 1):
+
+        dp[i] = 1 + dp[i - dp[dp[i - 1]]]
+
     return dp
 
 
