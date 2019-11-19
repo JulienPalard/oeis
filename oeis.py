681926cd5a71e4d29b1748e91059d2c0ec397f39
Anthony <anthonyd-m@live.com>
2019-11-19 11:27:47 +0100

    added A001220 function

--- oeis.py
+++ oeis.py
@@ -272,6 +272,30 @@ def partitions(n):
 def A000041(n):
     return len(partitions(n))
 
+@oeis
+def A001220(start, limit, plot):
+    sequence = []
+    for i in range(start, limit):
+        if is_prime(i) and (2 ** (i-1) - 1) % (i ** 2) == 0:
+            sequence.append(i)
+    return sequence
+
+def is_prime(n):
+    if n % 2 == 0:
+        return False
+    elif n % 3 == 0:
+        return False
+    elif n % 5 == 0:
+        return False
+    elif n % 7 == 0:
+        return False
+    elif n % 9 == 0:
+        return False
+    else:
+        for i in range(2, math.floor(math.sqrt(n))):
+            if n % i == 0:
+                return False
+        return True
 
 def main():
     args = parse_args()
