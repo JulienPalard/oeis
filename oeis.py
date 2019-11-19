6ac70044ea516ed29689f3df07b70e029b638b20
slay <decobert.a78@gmail.com>
2019-11-19 15:12:58 +0100

    --dark_plot added

--- oeis.py
+++ oeis.py
@@ -39,6 +39,10 @@ def parse_args():
         help="Define the starting point of the sequence (default: 0)",
     )
 
+    parser.add_argument(
+        "--dark_plot", action="store_true", help="Print a dark dark dark graph"
+    )
+
     return parser.parse_args()
 
 
@@ -360,6 +364,13 @@ def main():
     serie = oeis.series[args.sequence](args.start, args.limit)
     if args.plot:
         plt.scatter(list(range(len(serie))), serie)
+        plt.show()
+    elif args.dark_plot:
+        colors = []
+        for i in range(len(serie)):
+            colors.append(np.random.rand())
+        with plt.style.context("dark_background"):
+            plt.scatter(list(range(len(serie))), serie, s=50, c=colors, alpha=0.5,)
         plt.show()
     else:
         print(serie)
