330b2db2547a02481df1fb40158000e56533d306
slay <decobert.a78@gmail.com>
2019-11-19 15:36:27 +0100

    blacked

--- tests/test_A000045.py
+++ tests/test_A000045.py
@@ -2,6 +2,7 @@ from oeis import fibonacci
 from hypothesis import given
 from hypothesis.strategies import integers
 
+
 @given(integers(min_value=1, max_value=17))
 def test_A000045(x):
-    assert(fibonacci(x))
+    assert fibonacci(x)
