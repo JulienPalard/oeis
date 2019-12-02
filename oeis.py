f63d5c0c2464d76cf3c1a30ff92dbf4ce234519b
s4mking <samuel.simonney@golem.ai>
2019-12-02 11:52:20 +0100

    Add bell function and test

--- oeis.py
+++ oeis.py
@@ -283,6 +283,21 @@ def A008589(start: int = 0, limit: int = 20) -> Collection[int]:
     return sequence
 
 
+@oeis
+def A000110(start: int = 0, limit: int = 20) -> Collection[int]:
+    "Bell or exponential numbers: number of ways to partition a set of n labeled elements."
+    sequence = []
+    for n in range(start, limit):
+        bell = [[0 for i in range(n + 1)] for j in range(n + 1)]
+        bell[0][0] = 1
+        for i in range(1, n + 1):
+            bell[i][0] = bell[i - 1][i - 1]
+            for j in range(1, i + 1):
+                bell[i][j] = bell[i - 1][j - 1] + bell[i][j - 1]
+        sequence.append(bell[n][0])
+    return sequence
+
+
 def is_prime(n: int) -> bool:
     if n % 2 == 0:
         return False
