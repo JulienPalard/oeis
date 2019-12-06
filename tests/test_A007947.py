c5ee8849e038bb034e193a8f261892e46827f523
Alice Bonnet <alicebo.1717@gmail.com>
2019-12-06 15:47:10 +0100

    added more tests for A007947

--- tests/test_A007947.py
+++ tests/test_A007947.py
@@ -1,6 +1,7 @@
-from oeis import A007947
+from oeis import A007947, A000040
 from hypothesis import given
 from hypothesis.strategies import integers
+from functools import reduce
 
 
 def test_sequence():
@@ -28,6 +29,13 @@ def test_sequence():
     ]
 
 
-@given(integers(min_value=0, max_value=200), integers(min_value=1, max_value=200))
+@given(integers(min_value=0, max_value=2000), integers(min_value=1, max_value=2000))
 def test_sequence_length(start, limit):
     assert len(A007947(start, limit)) == limit
+
+
+@given(integers(min_value=0, max_value=2000), integers(min_value=1, max_value=2000))
+def test_A007947(start, limit):
+    composite = reduce(lambda x, y: x * y, A000040(start, limit), 1)
+    expected = reduce(lambda x, y: x * y, set(A000040(start, limit)), 1)
+    assert A007947(composite, limit=1) == [expected]
