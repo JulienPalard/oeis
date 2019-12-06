b694daac56bbedfa58fefdaed9a952ef6eaf6f4d
Julien Palard <julien@palard.fr>
2019-12-06 23:34:12 +0100

    Lower my CPU during tests.

--- tests/test_A007947.py
+++ tests/test_A007947.py
@@ -34,7 +34,7 @@ def test_sequence_length(start, limit):
     assert len(A007947(start, limit)) == limit
 
 
-@given(integers(min_value=0, max_value=2000), integers(min_value=1, max_value=2000))
+@given(integers(min_value=0, max_value=100), integers(min_value=1, max_value=100))
 def test_A007947(start, limit):
     composite = reduce(lambda x, y: x * y, A000040(start, limit), 1)
     expected = reduce(lambda x, y: x * y, set(A000040(start, limit)), 1)
