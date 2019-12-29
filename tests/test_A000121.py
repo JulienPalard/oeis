596bd2fe9ed3b0cde335c553330a518f721b1caf
danielsimonney <daniel.simonney60@gmail.com>
2019-12-29 12:13:29 +0100

    implement A000121

---
+++ tests/test_A000121.py
@@ -0,0 +1,12 @@
+from oeis import A000121
+from hypothesis import given
+from hypothesis.strategies import integers
+
+def test_sequence():
+    assert A000121(0,20)==[1, 2, 2, 3, 3, 3, 4,
+     3, 4, 5, 4, 5, 4, 4, 6, 5, 6, 6, 5, 6]
+
+@given(integers(min_value=0, max_value=200), integers(min_value=1, max_value=200))
+def test_sequence_length(start, limit):
+    assert len(A000121(start, limit)) == limit
+
