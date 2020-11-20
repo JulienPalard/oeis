f428f8d7afd0bf039a2ecdcab3b8445e03101e76
Olyno <olyno.dev@gmail.com>
2020-11-20 12:49:19 +0100

    :bug: Convert JS equal to Python

--- tests/test_A004767.py
+++ tests/test_A004767.py
@@ -4,4 +4,4 @@ from oeis import A004767
 def test_A004767(): 
     for value in A004767[:100]:
         assert value % 2 != 0
-        assert bin(value)[-2:] === '11'
+        assert bin(value)[-2:] == '11'
