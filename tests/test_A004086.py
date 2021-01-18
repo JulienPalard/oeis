e7c63ac23b250985824e70e34e0f3c27d8c05d97
Julien Palard <julien@palard.fr>
2021-01-18 17:52:54 +0100

    Don't overload deps: It's slower but ensures we use the pinned ones. (#291)

--- tests/test_A004086.py
+++ tests/test_A004086.py
@@ -1,5 +1,3 @@
-import random
-
 from oeis import A004086
 from hypothesis import given
 from hypothesis.strategies import integers
@@ -10,7 +8,6 @@ def test_sequence(n):
     assert A004086[A004086[n]] == int(str(n).rstrip("0"))
 
 
-def test_sequence_other_values():
-    for i in range(100):
-        result = random.choices("123456789", k=5)
-        assert A004086[int("".join(result))] == int("".join(reversed(result)))
+@given(integers(min_value=100000000, max_value=999999999))
+def test_sequence_other_values(n):
+    assert A004086[n] == int("".join(reversed(str(n))))
