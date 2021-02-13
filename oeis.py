232b11703dddc53ae330e65131247c5cc7b7f044
Alexander W. Janssen <alexander.janssen@gmail.com>
2021-02-13 17:59:42 +0100

    Implemented A002275, fixes #111

--- oeis.py
+++ oeis.py
@@ -639,6 +639,14 @@ def A007089(n: int) -> int:
 
 
 @oeis.from_function()
+def A002275(n: int) -> int:
+    """Repunits: (10^n - 1)/9. Often denoted by R_n."""
+    if n == 0:
+        return 0
+    return int("1" * n)
+
+
+@oeis.from_function()
 def A070939(i: int = 0) -> int:
     """Length of binary representation of n."""
     return len(f"{i:b}")
